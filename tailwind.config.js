138f1d1b4b93902dfd217b9bb7eb8d9ffa5f7128
Genimarcio <49964829+Genimarcio@users.noreply.github.com>
2023-03-18 18:40:50 -0300

    versão 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        color_Geral: '#f3ede5',
+        color_Letras_Header: '#6f6f6f',
+        color_botao: '#133416',
+      },
+    },
   },
   plugins: [],
 }
